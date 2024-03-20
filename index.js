e2af778123464924736bddffcdd951a2fe5a17cd
Chrispine Oburu <chrizoburu65@gmail.com>
2024-03-20 11:14:00 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+let num1 = 2
+let num2 = 31
+let num3 = 9
+let num4 = 5
+let num5 = 20
+const multiply = num1 * num2
+const random = Math.floor(Math.random() * 20) + 1
+const mod = num3 % num4
+const max = Math.max(...[num1, num5, num3, num4])
